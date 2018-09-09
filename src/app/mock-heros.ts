de461a258760d9231fc7929c42f102bdf1aedce8
Retrojb <baltes.26@osu.edu>
2018-09-08 23:40:55 -0400

    inital commit

---
+++ src/app/mock-heros.ts
@@ -0,0 +1,42 @@
+import {
+  Hero
+} from './hero';
+
+export const HEROES: Hero[] = [{
+    id: 1,
+    firstName: 'John',
+    lastName: 'Baltes',
+    salary: 60000
+  },
+  {
+    id: 2,
+    firstName: 'Bob',
+    lastName: 'Barker',
+    salary: 6000000
+  },
+  {
+    id: 3,
+    firstName: 'Norman',
+    lastName: 'Reedus',
+    salary: 100000
+  },
+  {
+    id: 4,
+    firstName: 'Chuck',
+    lastName: 'Norris',
+    salary: 60000000
+  },
+  {
+    id: 5,
+    firstName: 'Rigby',
+    lastName: 'Pupper',
+    salary: 0
+  },
+  {
+    id: 6,
+    firstName: 'Sterling',
+    lastName: 'Archer',
+    salary: 560983
+  }
+
+];
